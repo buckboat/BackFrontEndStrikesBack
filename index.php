2cbdd0de1cdc3f0a8ff3020d15f3e92eeb6dc3a6
buckboat <123005977+buckboat@users.noreply.github.com>
2024-02-17 22:01:43 -0600

    failures
    pushing code

--- index.php
+++ index.php
@@ -1,76 +1,102 @@
+<!doctype html>
 <html>
-	<head>
-		<title>Lumberjack Rewards</title>
-		<link rel="icon" type="image/x-icon" href="favicon.ico">
-	</head>
 
-	<body>
-		
-		<div class="container">
+<head>
+	<title>Lumberjack Rewards</title>
+	<link rel="icon" type="image/x-icon" href="favicon.ico">
+	<?php echo '<link rel="stylesheet" type="text/css" href="style.css"></head>'; ?>
+</head>
 
-			<h1 style="text-align:center">
 
-				<img style="img align=left" src="spirit-logo-purple-tn2.jpg" alt="" width="64" height="64">
-		
+
+<body>
+
+	<div class="container">
+
+		<h1 style="text-align:center">
+
+			<img src="spirit-logo-purple-tn2.jpg" alt="" width="64" height="64">
+
 			Lumberjack Rewards
 
-			</h1>
+		</h1>
+
+
+	</div>
+
+	<br />
+	<div class="container">
+		<div class="row">
+
+			<div class="col-sm-4">
+				<?php
+
+				include "leftside.php";
+
+				?>
+			</div>
+
+
+			<div class="col-sm-8">
+				/*
+				<?php
+
+		        
+				// Create connection
+				$conn = new mysqli("localhost", "test", "test", "");
+
+				// Check connection
+				if ($conn->connect_error) {
+					die("Connection failed: " . $conn->connect_error);
+				}
+
+				// Check if database exists
+				$checkDatabaseQuery = "SHOW DATABASES LIKE 'gfj_db'";
+				$result = $conn->query($checkDatabaseQuery);
+
+				if ($result->num_rows == 0) {
+					// The database does not exist, so create it and populate it
+					ob_start();
+					include('FISHPROJECT_DB_Table_Creation.php');
+					include('FISH_FILL_TABLES.php');
+					ob_end_clean();
+					//echo "<pre>gfj_db not found, creating and populating\n </pre>";
+				} else {
+					//echo "<pre>gfj_db found! no need to recreate and repopulate\n </pre>";
+				}
+				echo "<pre>database is ready\n </pre>";
+				?>
+
+			</div>
 
 
 		</div>
+	</div>
+	<div>
 
-		<br/>
-		
-		<?php
-			// Create connection
-			$conn = new mysqli("localhost","test", "test", "");
 
-			// Check connection
-			if ($conn->connect_error) {
-				die("Connection failed: " . $conn->connect_error);
-			}
-
-			// Check if database exists
-			$checkDatabaseQuery = "SHOW DATABASES LIKE 'gfj_db'";
-			$result = $conn->query($checkDatabaseQuery);
-
-			if ($result->num_rows == 0) {
-				// The database does not exist, so create it and populate it
-				ob_start();
-				include('FISHPROJECT_DB_Table_Creation.php'); 
-				include('FISH_FILL_TABLES.php');
-				ob_end_clean();
-				//echo "<pre>gfj_db not found, creating and populating\n </pre>";
-			}
-			else {
-				//echo "<pre>gfj_db found! no need to recreate and repopulate\n </pre>";
-			}
-			echo "<pre>database is ready\n </pre>";
-		?>		
-		
-		<!-- form for navigation/execution -->		
-		<form method="post"> 
-			<input type="submit" name="Login"
-                value="Login"/>
+		<!-- form for navigation/execution -->
+		<form method="post">
+			<input type="submit" name="Login" value="Login" />
 		</form>
-		
+
 		<div style="border: 4px solid black; padding: 5px;">
 
-		<!-- button functionality -->
-		<?php
-		if(isset($_POST['Login'])) { 
-			include('login.php');
-        } 
-		if(isset($_POST['B'])) { 
-            echo "<pre>reset button pressed\n </pre>"; 
-			$conn->query("DROP DATABASE gfj_db");
-			ob_start();
-				include('FISHPROJECT_DB_Table_Creation.php'); 
+			<!-- button functionality -->
+			<?php
+			if (isset($_POST['Login'])) {
+				include('login.php');
+			}
+			if (isset($_POST['B'])) {
+				echo "<pre>reset button pressed\n </pre>";
+				$conn->query("DROP DATABASE gfj_db");
+				ob_start();
+				include('FISHPROJECT_DB_Table_Creation.php');
 				include('FISH_FILL_TABLES.php');
-			ob_end_clean();
-			echo "<pre>gfj_db reset to default values!\n </pre>";
-        } 
-		?> 
+				ob_end_clean();
+				echo "<pre>gfj_db reset to default values!\n </pre>";
+			}
+			?>
 		</div>
 
 		<div>
@@ -79,6 +105,6 @@
 			<button onclick="location.href=''">Goes no where</button>
 		</div>
 
-	</body>
+</body>
 
 </html>
---
+++ leftside.php
@@ -0,0 +1,17 @@
+<html>
+
+<body>
+
+
+
+
+        <div class="w3-sidebar w3-bar-block" style="width:25%">
+  <a href="#" class="w3-bar-item w3-button">Link 1</a>
+  <a href="#" class="w3-bar-item w3-button">Link 2</a>
+  <a href="#" class="w3-bar-item w3-button">Link 3</a>
+</div>
+
+
+</body>
+
+</html>
