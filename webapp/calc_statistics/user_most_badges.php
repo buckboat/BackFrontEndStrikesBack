d7682effb1bad4fe702bbd6a4180bd5edf7f5f17
Fernando Perez <122562560+perezfj2@users.noreply.github.com>
2024-04-14 22:00:14 -0500

    Update user_most_badges.php

--- webapp/calc_statistics/user_most_badges.php
+++ webapp/calc_statistics/user_most_badges.php
@@ -50,41 +50,7 @@ mysqli_close($conn);
         }
     </style>
 </head>
-<body>
-
-<h1 style="text-align:center; color:white; ">
-
-    <img src="spirit-logo-purple-rgb.png" alt="" width="64" height="64">
-
-    Lumberjack Rewards
-
-</h1>
-
-<div class="main">
-    <div class="leftside">
-        <!-- Your leftside navigation code goes here -->
-        <form style="padding-bottom:5px;" method="post">
-            <input type="submit" name="mems" value="Members" />
-        </form>
-        <form style="padding-bottom:5px;" method="post">
-            <input type="submit" name="badges" value="Badges" />
-        </form>
-        <form style="padding-bottom:5px;" method="post">
-            <input type="submit" name="requests" value="Requests" />
-        </form>
-        <form style="padding-bottom:5px;" method="post">
-            <input type="submit" name="Stats" value="Statistics" />
-        </form>
-        <form style="padding-bottom:5px;" method="post">
-            <input type="submit" name="events" value="Events" />
-        </form>
-
-        <a href="https://bit.ly/3BlS71b"><img src="Donate.jpg" style="display:flex; vertical-align:middle; color:red; " width="100%" height="44"></a>
-
-        <form style=" padding-top:266%; bottom: 20px;" method="post">
-            <input type="submit" name="Logout" value="Logout" />
-        </form>
-    </div>
+<body> 
 
     <h2>Leaderboard - User with Most Badges</h2>
 
@@ -95,7 +61,6 @@ mysqli_close($conn);
             <li>No user found with badges</li>
         <?php endif; ?>
     </ul>
-</div>
 
 </body>
 </html>
