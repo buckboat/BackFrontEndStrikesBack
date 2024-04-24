0c465d1647e6f118a06ca18dd8c6cccf800da8f5
wilsonjj3 <122841036+wilsonjj3@users.noreply.github.com>
2024-04-24 01:58:22 -0500

    updated editbadge and addbadge
    Changed them to fit better into badgerequest table (hopefully). Also made ifs to check if a badge already has an update pending or not.

--- webapp/addbadge.php
+++ webapp/addbadge.php
@@ -10,28 +10,29 @@
         <?php
         if (isset($_POST['insertBadge'])) {
         
-             include "..//database_operations/DBConnection.php";
-             $engine = new DBConnection();
-             $conn = $engine->connect();
+            include "..//database_operations/DBConnection.php";
+            $engine = new DBConnection();
+            $conn = $engine->connect();
 
             $badge_name = $_POST['badge_name'];
             $badge_desc = $_POST['badge_desc'];
             $badge_criteria = $_POST['badge_criteria'];
             $badge_comment = $_POST['badge_comment'];
+            $badge_rarity = $_POST['badge_rarity'];
+            $badge_steps = $_POST['badge_steps'];
 
-            // Insert new badge into the database
-            $sql = "INSERT INTO BadgeRequest (BadgeName, BadgeDesc, BadgeCriteria, Comment, isVisible) 
-                    VALUES ('$badge_name', '$badge_desc', '$badge_criteria', '$badge_comment', 1)";
-            
+        // Insert new badge request into the database
+        $sql = "INSERT INTO BadgeRequest (UserID, BadgeName, BadgeDesc, BadgeCriteria, BadgeRarity, BadgeSteps, Comment, isVisible) 
+                VALUES ('{$_SESSION['UserID']}', '$badge_name', '$badge_desc', '$badge_criteria', '$badge_rarity', '$badge_steps', '$badge_comment', 1)";
 
-
-    if(mysqli_query($conn, $sql)) {
-                echo "New badge added successfully.";
-            } else {
-                echo "Error adding badge: " . mysqli_error($conn);
+        if(mysqli_query($conn, $sql)) {
+                    echo "New badge added successfully.";
+                } else {
+                    echo "Error adding badge: " . mysqli_error($conn);
+                }
             }
-        }
         ?>
+
         <label for="badge_name">Badge Name:</label><br>
         <input type="text" id="badge_name" name="badge_name" required><br><br>
 
@@ -43,6 +44,12 @@
         
         <label for="badge_comment">Comment:</label><br>
         <textarea id="badge_comment" name="badge_comment" rows="4" cols="50" required></textarea><br><br>
+
+        <label for="badge_rarity">Badge Rarity:</label><br>
+        <textarea id="badge_rarity" name="badge_rarity" rows="4" cols="50" required></textarea><br><br>
+
+        <label for="badge_steps">Badge Steps:</label><br>
+        <textarea id="badge_steps" name="badge_steps" rows="4" cols="50" required></textarea><br><br>
 
         <input type="submit" name="insertBadge" value="Add Badge Request">
     </form>
--- webapp/editbadge.php
+++ webapp/editbadge.php
@@ -36,28 +36,41 @@
             $badge_name = $badge['badge_name'];
             $badge_desc = $badge['badge_desc'];
             $badge_criteria = $badge['badge_criteria'];
+            $badge_comment = $badge['badge_comment'];
+            $badge_rarity = $badge['badge_rarity'];
+            $badge_steps = $badge['badge_steps'];
 
-
-            //needs a check to see if any changes from saved changes to whats in the database
-            // sql with badge id to pull data and check to see if any changes
-            // if no changes skip
-            // if changes another sql to see if a request with the same badge id exists if it does update the request table
-            // if no chages insert into the request table.
+            // Check if there are any mf changes from saved changes to what is in the database
+            $sql_check_changes = "SELECT * FROM Badge WHERE BadgeID = '$badge_id'";
+            $result_check_changes = mysqli_query($conn, $sql_check_changes);
+            $row_check_changes = mysqli_fetch_assoc($result_check_changes);
+        
+            if ($row_check_changes['BadgeName'] != $badge_name || $row_check_changes['BadgeDesc'] != $badge_desc || $row_check_changes['BadgeCriteria'] != $badge_criteria || $row_check_changes['BadgeComment'] != $badge_comment || $row_check_changes['BadgeRarity'] != $badge_rarity || $row_check_changes['BadgeSteps'] != $badge_steps) {
+                // Check if a mf request with the same badgeID exists
+                $sql_check_request = "SELECT * FROM BadgeRequest WHERE BadgeID = '$badge_id'";
+                $result_check_request = mysqli_query($conn, $sql_check_request);
             
-            // Update the badge in the database 
-            $sql = "UPDATE Badge SET BadgeName = '$badge_name', BadgeDesc = '$badge_desc', BadgeCriteria = '$badge_criteria' WHERE BadgeID = '$badge_id'";
-
-            if (mysqli_query($conn, $sql)) {
-                echo "Badge updated successfully for BadgeID: $badge_id.<br>";
+                if (mysqli_num_rows($result_check_request) > 0) {
+                    // Update the mf request table
+                    $sql_update_request = "UPDATE BadgeRequest SET BadgeName = '$badge_name', BadgeDesc = '$badge_desc', BadgeCriteria = '$badge_criteria', BadgeComment = '$badge_comment', BadgeRarity = '$badge_rarity', BadgeSteps = '$badge_steps' WHERE BadgeID = '$badge_id'";
+                    if (mysqli_query($conn, $sql_update_request)) {
+                        echo "Request updated successfully for BadgeID: $badge_id.<br>";
+                    } else {
+                        echo "Error updating request for BadgeID: $badge_id - " . mysqli_error($conn) . "<br>";
+                    }
+                } else {
+                    // Insert into the mf request table
+                    $sql_insert_request = "INSERT INTO BadgeRequest (UserID, BadgeID, BadgeName, BadgeDesc, BadgeCriteria, BadgeIcon, BadgeRarity, BadgeSteps, Comment, isVisible) VALUES ('{$_SESSION['UserID']}', '$badge_id', '$badge_name', '$badge_desc', '$badge_criteria', 1, 1, 1, '', 1)";
+                    if (mysqli_query($conn, $sql_insert_request)) {
+                        echo "New request added successfully for BadgeID: $badge_id.<br>";
+                    } else {
+                        echo "Error adding request for BadgeID: $badge_id - " . mysqli_error($conn) . "<br>";
+                    }
+                }
             } else {
-                echo "Error updating badge for BadgeID: $badge_id - " . mysqli_error($conn) . "<br>";
+                echo "No changes detected for BadgeID: $badge_id. Skipping...<br>";
             }
         }
-
-        // Redirect to index.php after updating badges
-        //header("Location: index.php");
-        
-        //exit();
     }
 
     // Fetch all badges from the database
@@ -71,6 +84,9 @@
                 <th>Badge Name</th>
                 <th>Badge Description</th>
                 <th>Badge Criteria</th>
+                <th>Badge Comment</th>
+                <th>Badge Rarity</th>
+                <th>Badge Steps</th>
             </tr>
             <?php
       
@@ -80,6 +96,9 @@
                 echo "<td><input type='text' name='edit[" . $row['BadgeID'] . "][badge_name]' value='" . $row['BadgeName'] . "' required></td>";
                 echo "<td><input type='text' name='edit[" . $row['BadgeID'] . "][badge_desc]' value='" . $row['BadgeDesc'] . "' required></td>";
                 echo "<td><input type='text' name='edit[" . $row['BadgeID'] . "][badge_criteria]' value='" . $row['BadgeCriteria'] . "' required></td>";
+                echo "<td><input type='text' name='edit[" . $row['BadgeID'] . "][badge_comment]' value='" . $row['BadgeComment'] . "' required></td>";
+                echo "<td><input type='text' name='edit[" . $row['BadgeID'] . "][badge_rarity]' value='" . $row['BadgeRarity'] . "' required></td>";
+                echo "<td><input type='text' name='edit[" . $row['BadgeID'] . "][badge_steps]' value='" . $row['BadgeSteps'] . "' required></td>";
                 echo "</tr>";
             }
             ?>
@@ -92,13 +111,10 @@
         <button type='submit'>Save Changes</button>
         <div></div>
         <div></div> <!--  master html ui design -->
-        
-
-
+        <!--  master of questioning why my smooth brain chose stem -->
 
     </form>
     <form method="post"><input type='submit' name='addBadge' value='Add Badge'></form>
 
 </body>
-
 </html>
